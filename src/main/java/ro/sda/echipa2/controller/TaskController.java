d207739435547aa419d2d0987996549a2512ebbd
djiolau <djiolau999@gmail.com>
2021-10-24 21:50:31 +0200

    adding entities,service,controller and repositories 4

--- src/main/java/ro/sda/echipa2/controller/TaskController.java
+++ src/main/java/ro/sda/echipa2/controller/TaskController.java
@@ -2,11 +2,13 @@ package ro.sda.echipa2.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import ro.sda.echipa2.service.TaskService;
 
 
 @Slf4j
+@Controller
 public class TaskController {
 
     @Autowired
--- src/main/java/ro/sda/echipa2/model/AppUserRole.java
+++
@@ -1,6 +0,0 @@
-package ro.sda.echipa2.model;
-
-public enum AppUserRole {
-    USER,
-    ADMIN
-}
