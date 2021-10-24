26f962a3cbdfaf0722e459561157c985c4657f91
Mihai <80095435+Jansky23@users.noreply.github.com>
2021-10-24 14:03:13 +0300

    commit

--- src/main/java/ro/sda/echipa2/controller/TaskController.java
+++ src/main/java/ro/sda/echipa2/controller/TaskController.java
@@ -2,15 +2,19 @@ package ro.sda.echipa2.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import ro.sda.echipa2.service.TaskService;
 
 
 @Slf4j
+@Controller
 public class TaskController {
 
     @Autowired
     TaskService taskService;
+    @GetMapping("/tasks")
     public String showTasks(Model model){
         model.addAttribute("tasks", taskService.findAll());
         return "tasks";
