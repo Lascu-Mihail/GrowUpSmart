f98c273834dffc6b9bb2cd3db39400b555724930
Mihai <80095435+Jansky23@users.noreply.github.com>
2021-10-30 10:00:54 +0300

    tasks si navbar , si versiuni

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,8 @@ insert into task_category values (1,'Task Category1 description','Task Category 
 insert into task values (1,null,'First Task that has a description',null,'First Task',null,1);
 insert into task values (2,null,'Second Task that has a description',null,'Second Task',null,1);
 insert into task values (3,null,'Third Task that has a description',null,'Third Task',null,1);
-insert into task values (4,null,'Forth Task that has a description',null,'Forth Task',null,1);
+insert into task values (5,null,'Forth Task that has a description',null,'Forth Task',null,1);
+insert into task values (6,null,'Forth Task that has a description',null,'Forth Task',null,1);
+insert into task values (7,null,'Forth Task that has a description',null,'Forth Task',null,1);
+insert into task values (8,null,'Forth Task that has a description',null,'Forth Task',null,1);
+insert into task values (9,null,'Forth Task that has a description',null,'Forth Task',null,1);
