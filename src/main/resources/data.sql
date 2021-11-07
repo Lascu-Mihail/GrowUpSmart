7afc726cae256ab6b56b6c15a11abac8a85c7511
djiolau <djiolau999@gmail.com>
2021-11-07 10:49:33 +0100

    Merged with main

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,8 +9,6 @@ insert into task values (8,null,'Forth Task that has a description',null,'Forth 
 insert into task values (9,null,'Forth Task that has a description',null,'Forth Task',null,1);
 
 -- parola1
-insert into user (id,email,first_name,password,user_role,username)
-values (1, 'admin@gmail.com', 'Admin', '$2a$10$SoH3pawKCTAlMjDA5GKPpOU3bFodACbndY6x4l49Z1WzfochfTT2O', 'ADMIN', 'admin@gmail.com');
+insert into user (id,email,first_name,password,user_role,username,locked,enabled) values (1, 'admin@gmail.com', 'Admin', '$2a$10$SoH3pawKCTAlMjDA5GKPpOU3bFodACbndY6x4l49Z1WzfochfTT2O', 'ADMIN', 'admin@gmail.com',false,true);
 -- love
-insert into user (id,email,first_name,password,user_role,username)
-values (2, 'user@gmail.com', 'User', '$2a$10$4pWDP/awmmmJHeXNndeYpeWLmdm9BFF5h5QtTlMP6Ezt0A0.oJpAm', 'USER', 'user@gmail.com');
+insert into user (id,email,first_name,password,user_role,username,locked,enabled) values (2, 'user@gmail.com', 'User', '$2a$10$4pWDP/awmmmJHeXNndeYpeWLmdm9BFF5h5QtTlMP6Ezt0A0.oJpAm', 'USER', 'user@gmail.com',false,true);
