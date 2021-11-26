268dec030971a3dcb133e3c5280db785d76728ca
Mihai <80095435+Jansky23@users.noreply.github.com>
2021-11-26 10:07:49 +0200

    registration css and html  completed !

--- src/main/java/ro/sda/echipa2/controller/UserController.java
+++ src/main/java/ro/sda/echipa2/controller/UserController.java
@@ -4,13 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import ro.sda.echipa2.model.User;
 import ro.sda.echipa2.repository.UserRepository;
 import ro.sda.echipa2.service.UserService;
-
 import javax.validation.Valid;
 import java.util.List;
 
